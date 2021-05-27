9457838dd9c51782b7c9a15bb96d3db5ae809f49
hyakimaro <>
2021-05-27 03:17:51 +0000

    OK

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ app.use((req, res, next) => {
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Autorization'
-		);
+	);
 
-		if (req.method === 'OPTIONS') {
-			res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-			return res.status(200).send({});
-		};
-		next();
+	if (req.method === 'OPTIONS') {
+		res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).send({});
+	};
+	next();
 });
 
 app.post('/', async (req, res) => {
 	const request = await rp({ 
-			method: req.body.method,
-			uri: req.body.url,
-			form: req.body.body,
-			headers: req.body.headers
+		method: req.body.method,
+		uri: req.body.url,
+		form: req.body.body,
+		headers: req.body.headers
 	})
   res.send(request);
 });
 
-app.get('/knowQuery.js', async (req, res) => {
-	res.sendFile( __dirname + "/lib/knowQuery.js" );
+app.get('/lib/:id_libs', async (req, res) => {
+	res.sendFile( __dirname + "/lib/" + req.params.id_libs );
 });
 
 app.get('/examples', async (req, res) => {
